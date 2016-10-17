695d342a9552510e92db5f88daea96e8d4676be6
Piers Roberts <piers.roberts@somoglobal.com>
2016-10-17 15:19:11 +0100

    Removed nonsense commented permissions

--- Plugin.php
+++ Plugin.php
@@ -30,31 +30,4 @@ class Plugin extends \System\Classes\PluginBase
             'BuzzwordCompliant\FAQs\Components\FAQ' => 'faq'
         ];
     }
-//
-//    public function registerPermissions()
-//    {
-//        return [
-//            'buzzwordcompliant.dbcontent.list_static_blocks' => [
-//                'label' => 'Show the static block list',
-//                'tab' => 'DBContent'
-//            ],
-//            'buzzwordcompliant.dbcontent.new_static_block' => [
-//                'label' => 'Add a static block',
-//                'tab' => 'DBContent'
-//            ],
-//            'buzzwordcompliant.dbcontent.edit_static_block' => [
-//                'label' => 'Save changes to an existing static block',
-//                'tab' => 'DBContent'
-//            ],
-//            'buzzwordcompliant.dbcontent.delete_static_block' => [
-//                'label' => 'Delete a static block',
-//                'tab' => 'DBContent'
-//            ],
-//            'buzzwordcompliant.dbcontent.change_key_names' => [
-//                'label' => 'Change the key name of a static block',
-//                'tab' => 'DBContent'
-//            ]
-//        ];
-//    }
-
 }
