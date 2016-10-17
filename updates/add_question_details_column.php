3009542f3c47cb11fc4fca2fecc8dad39cd4c4ee
Piers Roberts <piers.roberts@somoglobal.com>
2016-10-17 14:32:11 +0100

    Added question detail column and updated the example in the README

---
+++ updates/add_question_details_column.php
@@ -0,0 +1,23 @@
+<?php namespace BuzzwordCompliant\FAQs\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class AddQuestionDetailsColumn extends Migration
+{
+    public function up()
+    {
+        Schema::table('bc_questions', function($table)
+        {
+            $table->text('details')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('bc_questions', function($table)
+        {
+            $table->dropColumn('details');
+        });
+    }
+}
