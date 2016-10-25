469388e625563220f313c943ad894e79df57bed4
Szabó Gergő <gergo85@gmail.com>
2016-10-25 14:07:17 +0200

    Add homepage to plugin details

--- Plugin.php
+++ Plugin.php
@@ -1,4 +1,5 @@
 <?php namespace BuzzwordCompliant\FAQs;
+
 use Backend;
 
 class Plugin extends \System\Classes\PluginBase
@@ -6,9 +7,10 @@ class Plugin extends \System\Classes\PluginBase
     public function pluginDetails()
     {
         return [
-            'name' => 'FAQs',
-            'author' => 'piersroberts@gmail.com',
-            'icon' => 'icon-question'
+            'name'     => 'FAQs',
+            'author'   => 'piersroberts@gmail.com',
+            'icon'     => 'icon-question',
+            'homepage' => 'https://github.com/piersroberts/october-faqs'
         ];
     }
 
@@ -19,7 +21,7 @@ class Plugin extends \System\Classes\PluginBase
                 'label'       => 'FAQs',
                 'url'         => Backend::url('buzzwordcompliant/faqs/faqs'),
                 'iconSvg'     => '/plugins/buzzwordcompliant/faqs/assets/icon.svg',
-                'permissions' => ['buzzwordcompliant.faqs.*'],
+                'permissions' => ['buzzwordcompliant.faqs.*']
             ]
         ];
     }
@@ -27,35 +29,34 @@ class Plugin extends \System\Classes\PluginBase
     public function registerComponents()
     {
         return [
-            'BuzzwordCompliant\FAQs\Components\FAQ' => 'faq',
-            'BuzzwordCompliant\FAQs\Components\FAQList' => 'faqlist',
+            'BuzzwordCompliant\FAQs\Components\FAQ'     => 'faq',
+            'BuzzwordCompliant\FAQs\Components\FAQList' => 'faqlist'
         ];
     }
-//
+
 //    public function registerPermissions()
 //    {
 //        return [
 //            'buzzwordcompliant.dbcontent.list_static_blocks' => [
 //                'label' => 'Show the static block list',
-//                'tab' => 'DBContent'
+//                'tab'   => 'DBContent'
 //            ],
 //            'buzzwordcompliant.dbcontent.new_static_block' => [
 //                'label' => 'Add a static block',
-//                'tab' => 'DBContent'
+//                'tab'   => 'DBContent'
 //            ],
 //            'buzzwordcompliant.dbcontent.edit_static_block' => [
 //                'label' => 'Save changes to an existing static block',
-//                'tab' => 'DBContent'
+//                'tab'   => 'DBContent'
 //            ],
 //            'buzzwordcompliant.dbcontent.delete_static_block' => [
 //                'label' => 'Delete a static block',
-//                'tab' => 'DBContent'
+//                'tab'   => 'DBContent'
 //            ],
 //            'buzzwordcompliant.dbcontent.change_key_names' => [
 //                'label' => 'Change the key name of a static block',
-//                'tab' => 'DBContent'
+//                'tab'   => 'DBContent'
 //            ]
 //        ];
 //    }
-
 }
